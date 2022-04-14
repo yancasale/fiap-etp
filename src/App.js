00e799b0d7cadbae2c2cfcdf732e1e0bd63973e8
yancasale <yancasale@gmail.com>
2022-04-13 22:43:10 -0300

    asdasd

---
+++ src/App.js
@@ -0,0 +1,93 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+        <div className="header">
+          <div className="navbar">
+            <p>
+              Sobre mim
+            </p>
+            <p>
+              Formação
+            </p>
+            <p>
+              Experiencia
+            </p>
+            <p>
+              Hobbies
+            </p>
+            </div> 
+        </div>
+
+      <div className="main">
+        <div className="secao2">
+       </div>
+        <div className="secao1">
+          <h1>
+            Um pouco sobre mim
+          </h1>
+          <p>  
+          Olá ! Eu me chamo Yan Casale, moro no interior do Rio de Janeiro e trabalho com UX Design á mais de 5 anos. Eu sou uma pessoa muito que gosta muito do que faz, feliz com o trabalho e bastante empática. Meus objetivos de vida são simples, construir uma carreira, e me tornar um profissional cada vez melhor
+          </p>
+        </div>
+
+      </div>
+
+      <div className="main2">
+      <div className="secao1">
+          <h1>
+          Minha formação
+          </h1>
+          <p>
+          Eu atualmente estou cursando o terceiro semestre de Sistemas para Internet na Faculdade de informática e Administração Paulista (FIAP). O ano previsto para a conclusão do curso é para o final de 2022.          </p>
+        </div>
+
+       <div className="secao2">
+         <img src='urç'></img>
+          
+       </div>
+      </div>
+
+      <div className="main3">
+       <div className="secao2">
+          
+       </div>
+      <div className="secao1">
+          <h1>
+          Minhas Experiências
+          </h1>
+          <p>
+          Eu iniciei minha carreira como UX Designer no grupo FCamara durante 3 anos (2015 - 2018), após esse período eu entrei também como UX Designer para o time do PicPay.          </p>
+        </div>
+
+      </div>
+
+      <div className="main2">
+      <div className="secao1">
+          <h1>
+          Meus Hobbies          </h1>
+          <p>
+          Eu tenho vários hobbies que eu uso para distrair minha mente, um homem não se vive só de trabalho haha, o meu favorito é fotografia, eu costumo sair e procurar lugares bonitos para fotografar.          </p>
+        </div>
+
+       <div className="secao2">
+          
+       </div>
+      </div>
+
+      
+
+        
+
+      
+
+      
+      
+    </div>
+  );
+}
+
+export default App;
